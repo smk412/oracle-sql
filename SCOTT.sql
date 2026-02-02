97c8e864ef05bc484b34412ad04fda5570dd635b
MinKyu <whu1477@naver.com>
2026-02-02 10:21:54 +0900

    첫번째 커밋

---
+++ SCOTT.sql
@@ -0,0 +1,11 @@
+-- Oracle (DBMS) - version(21c) - xe (database명)
+-- user (scott) - 테이블
+-- Structured Query Language(SQL)
+SELECT studno, name  -- 컬럼명(전체)
+from student; -- 테이블
+
+
+-- 1) professor 테이블, 전체 컬럼 조회.
+select * from professor;
+
+
