bf7b579f5dd4f1d0c25fb59989d0c26acf18c09e
MinKyu <whu1477@naver.com>
2026-02-02 10:49:40 +0900

    쿼리수정

--- SCOTT.sql
+++ SCOTT.sql
@@ -1,11 +1,15 @@
 -- Oracle (DBMS) - version(21c) - xe (database명)
 -- user (scott) - 테이블
 -- Structured Query Language(SQL)
-SELECT studno, name  -- 컬럼명(전체)
+SELECT *  -- 컬럼명(전체)
 from student; -- 테이블
 
 
 -- 1) professor 테이블, 전체 컬럼 조회.
 select * from professor;
+
+-- 2) 학생 -> 학생번호, 이름, 학년
+select studno, name, grade
+from student;
 
 
